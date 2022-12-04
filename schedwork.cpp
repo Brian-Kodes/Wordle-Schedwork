7e2ae59d56cab9043775f76b6c2f1d9412894a2f
Brian Ko <brianyko@usc.edu>
2022-12-04 02:16:54 +0000

    Final Submission I Believe

--- schedwork.cpp
+++ schedwork.cpp
@@ -7,45 +7,18 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
 #include "schedwork.h"
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
-// worker ID if that is useful to your implementation.
-// Feel free to not use or delete.
-
-
-// comments from .h 
-/**
- * @brief Produces a work schedule given worker availability,
- *        the number of needed workers per day, and the maximum 
- *        shifts any single worker is allowed. Returns true
- *        and the valid schedule if a solution exists, and false
- *        otherwise. 
- * 
- * @param [in]  avail n x k vector of vectors (i.e. matrix) of the availability
- *                    of the k workers for each of the n days
- * @param [in]  dailyNeed Number of workers needed per day (aka d)
- * @param [in]  maxShifts Maximum shifts any worker is allowed over 
- *                        the n day period (aka m)
- * @param [out] sched n x d vector of vectors indicating the d workers
- *                    who are scheduled to work on each of the n days
- * @return true if a solution exists; sched contains the solution
- * @return false if no solution exists; sched is undefined (can be anything)
- */
-
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
 bool helperFunction (const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<int> numShiftsWorked, int row, int col);
 bool isValid (std::vector<int> numDaysWorked, const AvailabilityMatrix& avail, DailySchedule& sched, const size_t dailyNeed, const size_t maxShifts);
 
 
 
-// Add your implementation of schedule() and other helper functions here
 bool isValid (const AvailabilityMatrix& avail, DailySchedule& sched, const size_t dailyNeed, const size_t maxShifts)
 {
   for (int i = 0; i < (int)sched[0].size() - 1; i++)
@@ -78,17 +51,8 @@ bool isValid (const AvailabilityMatrix& avail, DailySchedule& sched, const size_
       return false;
     }
   }
-//check if each row of schewdule matrix has a certain number of 1s
-//cannot exceed max number of workers
-//check if each column is less than a certain number (counting number of 1s inside of row and column)
 return true;
 }
-
-//iterate through every worker
-// check the availability of each worker "true"
-//in that for loop check if worker has already worked their maxShifts
-//check if they've already worked on that day
-//if it works set sched to current worker
 
 bool helperFunction (const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<int> numShiftsWorked, int row, int col)
 {
@@ -99,81 +63,27 @@ bool helperFunction (const AvailabilityMatrix& avail, const size_t dailyNeed, co
   }
   if (row > (int)sched.size() - 1)
   {
-    return true; //leave this loop and its fone
+    return true; 
   }
-  //.size() will return number of rows it has
-  /*for (int i = 0; i < (int)avail[0].size(); i++) 
+
+
+for (int j = 0; j < (int)avail[0].size(); j++)
+{
+  if (avail[row][col] == true)
   {
-      for (int j = 0; j < (int)avail.size(); j++)
+    sched[col][row] = true;
+    if (isValid(avail, sched, dailyNeed, maxShifts))
     {
-      if (avail[row][j] == true)
-      {
-          sched[j][row] = true;
-          numShiftsWorked[i];
-        
-        if (!isValid(avail, sched, dailyNeed, maxShifts))
-        {
-        numShiftsWorked[j]--;
-        sched[j][row] = INVALID_ID;
-        }
-      }
-    }
-  }*/
-
-
-
-    for (int j = 0; j < (int)avail[0].size(); j++)
-    {
-      if (avail[row][col] == true)
-      {
-        sched[col][row] = true;
-        if (isValid(avail, sched, dailyNeed, maxShifts))
-        {
-        numShiftsWorked[j]++;
-        }
-        else
-        {
-        sched[col][row] = INVALID_ID;
-        }
-
-      }
-      numShiftsWorked[j]--;
-    }
-
-
- /*for (int i = 0; i < sched.size(); i++)
-  {
-    for (int j = 0; j < sched[i].size(); i++)
-    {
-      if ()
-    }
-  }*/
-  /*for (int i = 0; i < avail[row].size(); i++)
-  {
-    
-  }*/
-  //for (int i = 0; i < sched[row].size(); i++) //iterating through worked one etc;
-  //{
-    /*if (sched[row][i] != INVALID_ID)
-    {
-      helperFunction(avail, dailyNeed, maxShifts, sched, numDaysWorked, row, col+1);
+      numShiftsWorked[j]++;
     }
     else
     {
-      for (int j = 0; j < avail[j].size(); j++)
-      {
-        if (avail[row][j] == 1)
-        {
-          numDaysWorked[i] += 1;
-          //check if they've already worked on that day
-          //call helper function after I set sched = 1
-        }
-      }
-    }*/
+      sched[col][row] = INVALID_ID;
+    }
 
-  //}
-
-  //create vector of numdaysWorked that we will iterate througha dn change
+  }
+  numShiftsWorked[j]--;
+}
   return false;
 }
 
@@ -187,17 +97,10 @@ bool schedule(
 )
 {
   std::vector<std::vector<Worker_T>> tempVector;
- // final loop set sched value equal to true
- // days worked ++
- // recursive call
- // if it does not meet conidtion after recursive call
- // decrement daysWorked
- // set DaysWorked to "IV"
     if(avail.size() == 0U){
         return false;
     }
     sched.clear();
-    // Add your code below
   int row = 0;
   int col = 0;
   std:: vector <int> numShiftsWorked (avail[0].size(), 0);
@@ -207,7 +110,7 @@ bool schedule(
   }
   for (int i = 0; i < (int)avail.size() - 1; i++)
   {
-    std::vector<Worker_T> tempColumns; //(dailyNeed, INVALID_ID);
+    std::vector<Worker_T> tempColumns; 
     tempVector.push_back(tempColumns);
     for (int j = 0; j < dailyNeed ; j++)
     {
--- wordle.cpp
+++ wordle.cpp
@@ -1,21 +1,16 @@
 // For debugging
 #include <iostream>
-// For std::remove
-//#include <algorithm>
 #include <map>
 #include <set>
 #include <vector>
-#include <string.h> //added this
+#include <string.h>
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
  
  
-// Add prototypes of helper functions here
-//we want recursive function here
 void helperFunction(const std::string& floating, const std::set<std::string>& dict, unsigned int i, std::string possibleWord, set<char> &floatMethod, std::set<std::string> &results)
 {
- //vector <std::string> comboWords;
  int blankCount = 0;
  for (int j = 0; j < possibleWord.size(); j++)
    {
@@ -23,12 +18,7 @@ void helperFunction(const std::string& floating, const std::set<std::string>& di
      {
        blankCount += 1;
      }
-     // else
-     // {
-     //   ;
-     // }
    }
-  // base case: word is complete
  if (!blankCount) {
    if (dict.find(possibleWord) != dict.end()) {
      results.insert(possibleWord);
@@ -37,93 +27,30 @@ void helperFunction(const std::string& floating, const std::set<std::string>& di
  }
  
  vector<char>setAlphabet ({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'});
- // word is not complete
  if (possibleWord[i] != '-') {
    helperFunction(floating, dict, i + 1, possibleWord, floatMethod, results);
    return;
  }
- 
- // current letter is a dash
- // if there are still floating letters remaining
  std::set<char>::iterator it;
  if (floatMethod.size()) {
-   //for (int j = 0; j < floatMethod.size(); j++)
    for (it = floatMethod.begin(); it != floatMethod.end(); it++)
    {
-     //first, make temp vector equal to floatingMethod
-     //possibleWord[i] = floating[i]
-     //change temp to not include floating[i] delete
-     //recurse with temporary instead of floatingMethod and i+1
-     //make the change permanent by making temp = floatingMethod
      set<char> temp = floatMethod;
-     //vector<char>::iterator it = temp.begin();
      possibleWord[i] = *it;
-     //for (it = temp.begin(); it != temp.end(); it++)
-     /*while (it != temp.end())
-     {
-       if (*it == floating[j])
-       {
-         temp.erase(it);
-         it++;
-       }
-       else
-       {
-         it++;
-       }
-     }*/
      temp.erase(*it);
-     //temp.erase(std::find(temp.begin(), temp.end(), floatMethod[j]));
      helperFunction(floating, dict, i + 1, possibleWord, temp, results);
    }
  }
- 
- // no floating letters remaining
  if (blankCount > floatMethod.size()) {
    for (int k = 0; k < setAlphabet.size(); k++)
    {
      possibleWord[i] = setAlphabet[k];
      helperFunction(floating, dict, i + 1, possibleWord, floatMethod, results);
    }
-   possibleWord[i] = '-';
  }
-       //check if there are more blanks than floating character
-   // inside if statement iterate through a-z and then set the word at that index to a-z
-   //recurse with i+1
- 
-   //if i go from a-z and nothing works
-   //after if statement for blanks and characters
-   //reset the word at index[i] to dash
- 
- /*for (unsigned int i = 0; i < floating.size(); i++)
- {
-   for (unsigned int j = 0; j < in.size(); j++)
-   {
-     if (in[j] == '-')
-     {
-       possibleWord += floating[i];
-     }
-     else
-     //add the letter at that location
-     {
-       possibleWord += in[j];
-     }
-   }
- }*/
- /*for (it = dict.begin(); it! = dict.end(); it++)
- {
-   possibleWord = *it;
-   if (possibleWord.size() != sizeof(inString))
-   {
-     ;
-   }
-   else
-   {
-    
-   }
- }*/
+ possibleWord[i] = '-';
 }
  
-// Definition of primary wordle function
 std::set<std::string> wordle(
    const std::string& in,
    const std::string& floating,
@@ -132,7 +59,6 @@ std::set<std::string> wordle(
    char inString[in.length()];
    strcpy(inString, in.c_str());
    unsigned int i = 0;
-   //vector<char> floatMethod;
    set<char> floatMethod;
    std::string letterHelper = floating;
    set<std::string>:: iterator it;
@@ -140,62 +66,11 @@ std::set<std::string> wordle(
    std::set<std::string> results;
    for (unsigned int i = 0; i < floating.size(); i++)
    {
-     //floatMethod.push_back(floating[i]);
      floatMethod.insert(floating[i]);
    }
    helperFunction(floating, dict, i, in, floatMethod, results);
-   // Add your code here
- /*  for (it = dict.begin(); it != dict.end(); it++)
-   {
-     possibleWord = *it;
-       if (possibleWord.size() != sizeof(inString))
-       {
-         ;
-       }
-       else
-       {//same size
-         for (unsigned int i = 0; i < possibleWord.length() - 1; i++) //its same size
-         {
-           if (inString[i] == possibleWord[i])
-           {
-             //helperFunction(possibleWord[i], inString[i], )
-             for (unsigned int j = 0; j < letterHelper.length(); j++)
-             {
-               /*for (unsigned int k = 0; k < possibleWord.length(); k++)
-               {
-                 if (possibleWord[k] ==)
- 
-               }*/
-               /*if (possibleWord.find(letterHelper[j]) != string::npos)
-               {
-                 probableWords.insert(possibleWord);
-               }
-               else
-               {
-                 ;
-               }
-             }
-           }
-           else
-           {
-             ;
-           }
-          
-         }*/
-         /*for (unsigned int i = 0; i < letterHelper.length(); i++)
-         {
-           for (unsigned int j = 0; j < possibleWord.length(); j++)
-           {
-             if (possibleWord)
-           }
-         }*/
-           return results;
- }
- 
- 
- 
-// Define any helper functions here
- 
+   return results;
+} 
 
 
 
